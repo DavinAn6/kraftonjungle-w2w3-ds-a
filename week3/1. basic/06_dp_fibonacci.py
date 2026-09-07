a1526d97bf0d79f292f71ed3382450c40ef07ff4
Davin An <blueapple1030@gmail.com>
2026-09-07 15:53:30 +0900

    Updated for simpler code : Basic 06_dp_fibonacci.py

--- week3/1. basic/06_dp_fibonacci.py
+++ week3/1. basic/06_dp_fibonacci.py
@@ -86,22 +86,15 @@ def fibonacci_memo(n, memo=None):
         memo = {}
     
     # TODO: base case 
-    if n < 0:
+    if n <= 1:
         return n
-    elif n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    
     
     # TODO: 이미 계산한 값이 memo에 있으면 반환
     if n in memo:
         return memo[n]
     
     # TODO: 재귀 호출하여 계산하고 memo에 저장
-    memo[n-1] = fibonacci_memo(n-1, memo)
-    memo[n-2] = fibonacci_memo(n-2, memo)
-    memo[n] = memo[n-1] + memo[n-2]
+    memo[n] = fibonacci_memo(n-1, memo) + fibonacci_memo(n-2, memo)
     return memo[n]
 
 
