11057c8c0ce1fc83bf54748ebf363b9d8fccdf22
Davin An <blueapple1030@gmail.com>
2026-09-08 15:10:18 +0900

    Updated : Basic 07_dp_stairs.py

--- week3/1. basic/07_dp_stairs.py
+++ week3/1. basic/07_dp_stairs.py
@@ -48,8 +48,7 @@ def climb_stairs(n):
         n번째 계단까지 오르는 방법의 수
     """
     # TODO: 특별한 경우 처리    
-    if n < 0:
-        return n
+    if n < 0: return 0
     
     # TODO: dp 배열 생성 및 초기화
     dp = [0] * (n+1)
